25062a0b2f2d5d370c4e45edb9c1ca03e42bd566
subhashiscse <smollickcseiu@gmail.com>
2025-11-06 03:53:18 +0000

    Update

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+public interface IDataProcesser
+{
+    void ProcessData(string procesedData);
+}
+
+public class JsonDataProceser : IDataProcesser
+{
+    public void ProcessData(string jsonData)
+    {
+        Console.WriteLine("Processing JSON data");
+        Console.WriteLine(jsonData);
+    }
+}
+
+public class XmlDataProvider{
+    public string GetXmlData()
+    {
+        XDocument xmlDocc = new XDocument(
+            new XElement("User", 
+                new XElement("Name", "Subhashis"),
+                new XElement("Age", 19),
+                new XElement("Country", "Bangladesh")
+            )
+            );
+        return xmlDocc.ToString(); 
+    }
+}
+
+
+public class XmlToJsonAdapter : IDataProcesser
+{
+    private readonly XmlDataProvider _xmlDataProvider;
+
+    public XmlToJsonAdapter(XmlDataProvider xmlProvider)
+    {
+        _xmlDataProvider = xmlProvider;
+    }
+    public void ProcessData(string procesedData)
+    {
+        string xmlData = _xmlDataProvider.GetXmlData();
+        
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(xmlData);
+        string convertedJson = Newtonsoft.Json.JsonConvert.SerializeXNode(doc, Newtonsoft.Json.Formatting.Indented, 
+            true);
+
+        Console.WriteLine(convertedJson);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ DectoratorPattern/DectoratorPattern/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+public interface ICoffee
+{
+    string GetDescription();
+    double GetCost();
+}
+
+public class SimpleCoffe : ICoffee{
+    public string GetDescription()
+    {
+        return "Simple Coffee";
+    }
+
+    public double GetCost()
+    {
+        return 100.0;
+    }
+}
+
+public abstract class CoffeeDectorator : ICoffee
+{
+    public ICoffee _dectoratedCoffee;
+
+    public CoffeeDectorator(ICoffee coffee)
+    {
+        _dectoratedCoffee = coffee;
+    }
+    
+    public virtual string GetDescription()
+    {
+        return _dectoratedCoffee.GetDescription();
+    }
+
+    public virtual double GetCost()
+    {
+        return _dectoratedCoffee.GetCost();
+    }
+}
+
+
+public class MilkDectorator : CoffeeDectorator
+{
+    public MilkDectorator(ICoffee coffee) : base(coffee)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return _dectoratedCoffee.GetDescription() + " Milk";
+    }
+
+    public override double GetCost()
+    {
+        return _dectoratedCoffee.GetCost() + 100;
+    }
+}
+
+public class SugarDectorator : CoffeeDectorator
+{
+    public SugarDectorator(ICoffee coffee) : base(coffee)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return _dectoratedCoffee.GetDescription() + " Sugar";
+    }
+
+    public override double GetCost()
+    {
+        return _dectoratedCoffee.GetCost() + 50;
+    }
+}
+public class HorilicsDectorator : CoffeeDectorator
+{
+    public HorilicsDectorator(ICoffee coffee) : base(coffee)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return _dectoratedCoffee.GetDescription() + " Horlics";
+    }
+
+    public override double GetCost()
+    {
+        return _dectoratedCoffee.GetCost() + 150;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICoffee coffee = new SimpleCoffe();
+        Console.WriteLine($"{coffee.GetDescription()} - {coffee.GetCost()}");
+        
+        coffee = new MilkDectorator(coffee);
+        Console.WriteLine($"{coffee.GetDescription()} - {coffee.GetCost()}");
+        coffee = new HorilicsDectorator(coffee);
+        Console.WriteLine($"{coffee.GetDescription()} - {coffee.GetCost()}");
+        //coffee = new SugarDectorator(coffee);
+        //Console.WriteLine($"{coffee.GetDescription()} - {coffee.GetCost()}");
+        
+        ICoffee coffee1 = new SimpleCoffe();
+        coffee1 = new MilkDectorator(coffee1);
+        Console.WriteLine($"{coffee1.GetDescription()} - {coffee1.GetCost()}");
+        coffee1 = new SugarDectorator(coffee1);
+        Console.WriteLine($"{coffee1.GetDescription()} - {coffee1.GetCost()}");
+    }
+}
